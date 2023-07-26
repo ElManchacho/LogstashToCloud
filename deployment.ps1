a8431c43368c676db7c9df1bdd76eb3d97017a77
AL1062886 <paul.leroyducardonnoy@cgi.com>
2023-07-26 15:40:59 +0200

    removed trashy stuff

--- deployment.ps1
+++ deployment.ps1
@@ -4,7 +4,7 @@ $file = "elastic-agent-8.8.2-windows-x86_64"
 
 $zipFile = "$file.zip"
 
-$enrollementToken = "Wmt0R2tZa0JuRTZSYnY3UGYtNGY6WWhyems4VnZUeE9Qc2VZUmNxV3gzUQ=="
+$enrollementToken = "<enrollement_token>"
 
 $location = (Get-Location).Path
 
@@ -30,45 +30,44 @@ sc.exe delete "Elastic Agent"
 Expand-Archive -Path $location"\$zipFile" -DestinationPath $location
 cd $file
 .\elastic-agent.exe install `
-  --fleet-server-es=https://f8f4b3970a5e45d387eea4cdcbf5ec23.eu-west-3.aws.elastic-cloud.com:443 `
-  --fleet-server-service-token=AAEAAWVsYXN0aWMvZmxlZXQtc2VydmVyL3Rva2VuLTE2OTAzNzYxMjg2NzY6eHQ3TTM4NGFRU3FXd05Rbk1FMDlsUQ `
-  --fleet-server-policy=44ceca40-2bb2-11ee-8cd6-b70a4ff5323c `
+  --fleet-server-es=<endpoint> `
+  --fleet-server-service-token=<token> `
+  --fleet-server-policy=<policy-name-or-id> `
   --fleet-server-port=8220
 
 #-----------------------------------------------------------------#
 
 # Check if zip file has already been downloaded
 
-# $existingZipFile = Test-Path $zipFile
+$existingZipFile = Test-Path $zipFile
 
+#----------------------------------------------------------------#
+###### This part of the script comes from the Elastic Agent ######
+######      integration commands provided by Elastic        ######
+#----------------------------------------------------------------#
 
-# #----------------------------------------------------------------#
-# ###### This part of the script comes from the Elastic Agent ######
-# ######      integration commands provided by Elastic        ######
-# #----------------------------------------------------------------#
+# Get 8.8.2 version of zipped elastic-agent
 
-# # Get 8.8.2 version of zipped elastic-agent
+if ( !$existingZipFile )
+{
+    Invoke-WebRequest -Uri "https://artifacts.elastic.co/downloads/beats/elastic-agent/$zipFile" -OutFile $zipFile
+}
 
-# if ( !$existingZipFile )
-# {
-#     Invoke-WebRequest -Uri "https://artifacts.elastic.co/downloads/beats/elastic-agent/$zipFile" -OutFile $zipFile
-# }
+# Ask to delete existing agent or not
 
-# # Ask to delete existing agent or not
+sc.exe delete "Elastic Agent"
 
-# sc.exe delete "Elastic Agent"
+Remove-Item $location"\$file"
 
-# Remove-Item $location"\$file"
+# Unzip it
 
-# # Unzip it
+Expand-Archive -Path $location"\$zipFile" -DestinationPath $location
 
-# Expand-Archive -Path $location"\$zipFile" -DestinationPath $location
+# Go into the newly unzipped elastic agent folder and install it
 
-# # Go into the newly unzipped elastic agent folder and install it
+cd $file
 
-# cd $file
+.\elastic-agent.exe install --url=https://<endpoint> --enrollment-token=$enrollementToken
 
-# .\elastic-agent.exe install --url=https://df6095956c724821a05a4fed766f6958.fleet.eu-west-3.aws.elastic-cloud.com:443 --enrollment-token=$enrollementToken
-
-# #----------------------------------------------------------------#
+#----------------------------------------------------------------#
 
