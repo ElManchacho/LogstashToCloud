dc5765bd6c7286e0178ad350bf212d093250837c
AL1062886 <paul.leroyducardonnoy@cgi.com>
2023-07-26 11:12:14 +0200

    ignore unzipped file + deployment file progress

--- deployment.ps1
+++ deployment.ps1
@@ -1,8 +1,3 @@
-#----------------------------------------------------------------#
-###### This part of the script comes from the Elastic Agent ######
-######      integration commands provided by Elastic        ######
-#----------------------------------------------------------------#
-
 # Define global variables
 
 $file = "elastic-agent-8.8.2-windows-x86_64"
@@ -11,15 +6,29 @@ $zipFile = "$file.zip"
 
 $enrollementToken = "Wmt0R2tZa0JuRTZSYnY3UGYtNGY6WWhyems4VnZUeE9Qc2VZUmNxV3gzUQ=="
 
+$location = (Get-Location).Path
+
+# Check if zip file has already been downloaded
+
+$existingZipFile = Test-Path $zipFile
+
+#----------------------------------------------------------------#
+###### This part of the script comes from the Elastic Agent ######
+######      integration commands provided by Elastic        ######
+#----------------------------------------------------------------#
+
 #$ProgressPreference = 'SilentlyContinue'
 
 # Get 8.8.2 version of zipped elastic-agent
 
-Invoke-WebRequest -Uri "https://artifacts.elastic.co/downloads/beats/elastic-agent/$zipFile" -OutFile $zipFile
+if ( !$existingZipFile )
+{
+    Invoke-WebRequest -Uri "https://artifacts.elastic.co/downloads/beats/elastic-agent/$zipFile" -OutFile $zipFile
+}
 
 # Unzip it
 
-Expand-Archive .\$zipFile -DestinationPath .$file
+Expand-Archive -Path $location"\$zipFile" -DestinationPath $location
 
 # Go into the newly unzipped elastic agent folder and install it
 
@@ -28,3 +37,4 @@ cd $file
 .\elastic-agent.exe install --url=https://df6095956c724821a05a4fed766f6958.fleet.eu-west-3.aws.elastic-cloud.com:443 --enrollment-token=$enrollementToken
 
 #----------------------------------------------------------------#
+
